bb1c08ebd6d39efc691b72c6a77e5ece6b09e85e
Anas Raza <anasr12003@gmail.com>
2026-07-05 00:35:31 +0530

    rollback app.js

--- app.js
+++ app.js
@@ -11,9 +11,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-app.get("/version", (req, res) => {
-    res.send("Version 2");
-});
-app.get("/health", (req, res) => {
-    res.send("Healthy");
-});
