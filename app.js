2b0a952e260dbd253782da561f77a1fea11b2847
Anas Raza <anasr12003@gmail.com>
2026-07-04 23:54:01 +0530

    Webhook test

--- app.js
+++ app.js
@@ -11,6 +11,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-app.get("/version", (req, res) => {
-    res.send("Version 2");
-});
